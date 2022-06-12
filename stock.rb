c716bd4060aac3515058ed8984cd63f277da452b
Roman Geathers <romangeathersdev@gmail.com>
2022-06-12 10:46:21 -0400

    Further refractor

--- stock.rb
+++ stock.rb
@@ -6,8 +6,7 @@ def stock_picker(prices)
         for sell_price in (idx + 1)..(prices.length - 1)
             if prices[sell_price] - buy_price > highest_profit
                 highest_profit = prices[sell_price] - buy_price
-                buy_sell[0] = idx
-                buy_sell[1] = sell_price
+                buy_sell = [idx, sell_price]
             end
         end
     end
