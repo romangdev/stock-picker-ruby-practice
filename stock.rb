d9e57c2dd00ae277a7f4f1e30865deb33a76d664
Roman Geathers <romangeathersdev@gmail.com>
2022-06-12 10:45:07 -0400

    Refractor to make a bit shorter

--- stock.rb
+++ stock.rb
@@ -1,22 +1,20 @@
 def stock_picker(prices)
     highest_profit = 0
-    buy_day = 0
-    sell_day = 0
     buy_sell = []
     prices.each_with_index do
         |buy_price, idx|
         for sell_price in (idx + 1)..(prices.length - 1)
             if prices[sell_price] - buy_price > highest_profit
                 highest_profit = prices[sell_price] - buy_price
-                buy_day = idx
-                sell_day = sell_price
+                buy_sell[0] = idx
+                buy_sell[1] = sell_price
             end
         end
     end
-    buy_sell << buy_day << sell_day
     buy_sell
 end
 
 #testing to see if it works properly
 check = stock_picker([17,3,6,9,15,8,6,1,10])
 p check
+# => [1, 4]
