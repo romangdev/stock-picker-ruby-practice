dfb0b7c1ee57f081061c2177bcce7b754202d829
Roman Geathers <romangeathersdev@gmail.com>
2022-06-12 10:33:31 -0400

    Practice ruby making stock picker method

---
+++ stock.rb
@@ -0,0 +1,22 @@
+def stock_picker(prices)
+    highest_profit = 0
+    buy_day = 0
+    sell_day = 0
+    buy_sell = []
+    prices.each_with_index do
+        |buy_price, idx|
+        for sell_price in (idx + 1)..(prices.length - 1)
+            if prices[sell_price] - buy_price > highest_profit
+                highest_profit = prices[sell_price] - buy_price
+                buy_day = idx
+                sell_day = sell_price
+            end
+        end
+    end
+    buy_sell << buy_day << sell_day
+    buy_sell
+end
+
+#testing to see if it works properly
+check = stock_picker([17,3,6,9,15,8,6,1,10])
+p check
